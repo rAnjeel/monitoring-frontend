8143dacc90736b25b9aa087d42486a1d02c37fb9
Angelo RABEARISON <151778901+rAnjeel@users.noreply.github.com>
2025-09-02 16:37:43 +0300

    Adaptation du vue
    Changer toute les classes bootstrap 5 en bootstrap 3.4.1

--- src/App.vue
+++ src/App.vue
@@ -4,43 +4,44 @@
 
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4 text-uppercase fixed-top">
-      <a class="navbar-brand fw-bold" href="/">Monitoring</a>
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <!-- Logo / Brand -->
+        <div class="navbar-header">
+          <button
+            type="button"
+            class="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#navbarContent"
+            aria-expanded="false"
+          >
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">Monitoring</a>
+        </div>
 
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarContent"
-        aria-controls="navbarContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarContent">
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <router-link to="/" class="nav-link active" href="#"><i class="bi bi-house-door"></i>Accueil</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/failed-logins" class="nav-link" href="#"><i class="bi bi-server"></i>Logs</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/import-csv" class="nav-link" href="#"><i class="bi bi-arrow-down"></i>Import CSV</router-link>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#"><i class="bi bi-gear"></i> Settings</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-danger" href="#"><i class="bi bi-box-arrow-right"></i> Déconnexion</a>
-          </li>
-        </ul>
+        <!-- Liens du menu -->
+        <div class="collapse navbar-collapse" id="navbarContent">
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+              <router-link to="/" class="nav-link">
+                <i class="bi bi-house-door"></i> Accueil
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/import-csv" class="nav-link">
+                <i class="bi bi-arrow-down"></i> Import CSV
+              </router-link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
 
-    <!-- C’est ici que Vue affiche la page en fonction de la route -->
+    <!-- Zone de rendu des routes -->
     <router-view />
   </div>
 </template>
