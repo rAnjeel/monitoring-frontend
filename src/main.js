aa035ec052d740b22ad63f3260fcdb70ac5b4265
rAnjeel <rabearisonangelo21@gmail.com>
2025-09-05 14:30:51 +0300

    delete vite

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router';
-
-// All Community Features
-import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
-ModuleRegistry.registerModules([AllCommunityModule]);
-
-createApp(App).use(router).mount('#app')
